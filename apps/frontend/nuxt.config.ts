862ecc59758f1d812261c8198e3c0da65ed95dc2
Her bunheng <brodara114@gmail.com>
2026-08-03 11:17:40 +0700

    commit

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
   nitro: {
     preset:
       process.env.NITRO_PRESET ||
-      (process.env.NODE_ENV === "production" ? "cloudflare-pages" : undefined),
+      (process.env.NODE_ENV === "production" ? "cloudflare-module" : undefined),
     prerender: {
       crawlLinks: false,
       routes: [],
